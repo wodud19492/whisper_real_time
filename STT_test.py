6a57fafb8b0f00bee9417aae4f96956a2b461a25
wodud19492 <lee19492@sogang.ac.kr>
2024-09-05 14:17:52 +0900

    new file:   STT_test.py 	modified:   transcribe_demo.py

--- STT_test.py
+++ STT_test.py
@@ -1,5 +1,3 @@
-#! python3.7
-
 import argparse
 import os
 import numpy as np
@@ -12,8 +10,7 @@ from queue import Queue
 from time import sleep
 from sys import platform
 
-
-def main():
+def STT():
     parser = argparse.ArgumentParser()
     parser.add_argument("--model", default="medium", help="Model to use",
                         choices=["tiny", "base", "small", "medium", "large"])
@@ -27,25 +24,22 @@ def main():
                         help="How much empty space between recordings before we "
                              "consider it a new line in the transcription.", type=float)
     if 'linux' in platform:
-        parser.add_argument("--default_microphone", default='list',
+        parser.add_argument("--default_microphone", default='pulse',
                             help="Default microphone name for SpeechRecognition. "
                                  "Run this with 'list' to view available Microphones.", type=str)
     args = parser.parse_args()
 
-    # The last time a recording was retrieved from the queue.
     phrase_time = None
-    # Thread safe Queue for passing data from the threaded recording callback.
     data_queue = Queue()
-    # We use SpeechRecognizer to record our audio because it has a nice feature where it can detect when speech ends.
     recorder = sr.Recognizer()
     recorder.energy_threshold = args.energy_threshold
-    # Definitely do this, dynamic energy compensation lowers the energy threshold dramatically to a point where the SpeechRecognizer never stops recording.
     recorder.dynamic_energy_threshold = False
 
-    # Important for linux users.
-    # Prevents permanent application hang and crash by using the wrong Microphone
+    source = None
+
     if 'linux' in platform:
         mic_name = args.default_microphone
+        print("mic name : ", mic_name)
         if not mic_name or mic_name == 'list':
             print("Available microphone devices are: ")
             for index, name in enumerate(sr.Microphone.list_microphone_names()):
@@ -53,91 +47,90 @@ def main():
             return
         else:
             for index, name in enumerate(sr.Microphone.list_microphone_names()):
-                if mic_name in name:
+                if mic_name == "pulse":
                     source = sr.Microphone(sample_rate=16000, device_index=index)
                     break
+            if source is None:  # 마이크가 설정되지 않았을 경우
+                raise ValueError(f"No matching microphone found for \"{mic_name}\".")
     else:
         source = sr.Microphone(sample_rate=16000)
 
-    # Load / Download model
     model = args.model
     if args.model != "tiny" and not args.non_english:
         model = model + ".en"
-    audio_model = whisper.load_model(model)
+
+    # 모델을 GPU로 로드
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    try:
+        audio_model = whisper.load_model(model).to(device)
+        if device == 'cuda':
+            print("GPU loaded")
+        else:
+            print("CPU loaded")
+    except RuntimeError as e:
+        if 'CUDA out of memory' in str(e):
+            print("CUDA out of memory error encountered. Switching to CPU.")
+            device = 'cpu'
+            audio_model = whisper.load_model(model).to(device)
+        else:
+            raise e
 
     record_timeout = args.record_timeout
     phrase_timeout = args.phrase_timeout
 
     transcription = ['']
 
-    with source:
-        recorder.adjust_for_ambient_noise(source)
+    if source is not None:
+        with source:
+            recorder.adjust_for_ambient_noise(source)
+    else:
+        raise RuntimeError("Microphone source is not initialized.")
 
-    def record_callback(_, audio:sr.AudioData) -> None:
-        """
-        Threaded callback function to receive audio data when recordings finish.
-        audio: An AudioData containing the recorded bytes.
-        """
-        # Grab the raw bytes and push it into the thread safe queue.
+    def record_callback(_, audio: sr.AudioData) -> None:
         data = audio.get_raw_data()
         data_queue.put(data)
 
-    # Create a background thread that will pass us raw audio bytes.
-    # We could do this manually but SpeechRecognizer provides a nice helper.
     recorder.listen_in_background(source, record_callback, phrase_time_limit=record_timeout)
 
-    # Cue the user that we're ready to go.
     print("Model loaded.\n")
 
     while True:
         try:
-            now = datetime.utcnow()
-            # Pull raw recorded audio from the queue.
+            now = datetime.now()
             if not data_queue.empty():
                 phrase_complete = False
-                # If enough time has passed between recordings, consider the phrase complete.
-                # Clear the current working audio buffer to start over with the new data.
                 if phrase_time and now - phrase_time > timedelta(seconds=phrase_timeout):
                     phrase_complete = True
-                # This is the last time we received new audio data from the queue.
                 phrase_time = now
                 
-                # Combine audio data from queue
                 audio_data = b''.join(data_queue.queue)
                 data_queue.queue.clear()
                 
-                # Convert in-ram buffer to something the model can use directly without needing a temp file.
-                # Convert data from 16 bit wide integers to floating point with a width of 32 bits.
-                # Clamp the audio stream frequency to a PCM wavelength compatible default of 32768hz max.
                 audio_np = np.frombuffer(audio_data, dtype=np.int16).astype(np.float32) / 32768.0
 
-                # Read the transcription.
-                result = audio_model.transcribe(audio_np, fp16=torch.cuda.is_available())
-                text = result['text'].strip()
+                # 입력 데이터를 GPU로 이동
+                audio_np = torch.tensor(audio_np).to(device)
+                print("audio data loaded : ", datetime.now())
 
-                # If we detected a pause between recordings, add a new item to our transcription.
-                # Otherwise edit the existing one.
+                # STT 수행 (GPU 강제 사용)
+                result = audio_model.transcribe(audio_np, fp16=(device == 'cuda'))
+
+                text = result['text'].strip()
+                print("text generated : ", datetime.now())
+
                 if phrase_complete:
                     transcription.append(text)
                 else:
                     transcription[-1] = text
 
-                # Clear the console to reprint the updated transcription.
-                os.system('cls' if os.name=='nt' else 'clear')
                 for line in transcription:
                     print(line)
-                # Flush stdout.
                 print('', end='', flush=True)
             else:
-                # Infinite loops are bad for processors, must sleep.
                 sleep(0.25)
         except KeyboardInterrupt:
             break
 
-    print("\n\nTranscription:")
-    for line in transcription:
-        print(line)
+    return text
 
-
-if __name__ == "__main__":
-    main()
+STT()
